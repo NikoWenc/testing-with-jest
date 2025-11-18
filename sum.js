36906c7399fba9fddb18b12b8b0cc53ccef5a780
NikoWenc <96979773+NikoWenc@users.noreply.github.com>
2025-11-18 16:06:50 +0800

    Create Calculator Class and test

--- sum.js
+++ sum.js
@@ -20,4 +20,23 @@ export function reverseString(string) {
     };
     
     return newString;
-}
+};
+
+export class Calculator {
+
+    add(num1, num2){
+        return num1 + num2;
+    };
+
+    sub(num1, num2){
+        return num1 - num2;
+    };
+
+    mul(num1, num2){
+        return num1 * num2;
+    };
+
+    div(num1, num2){
+        return num1 / num2;
+    };
+};
--- sum.test.js
+++ sum.test.js
@@ -1,6 +1,7 @@
 import { sum } from "./sum";
 import { capitalized } from "./sum";
 import { reverseString } from "./sum";
+import { Calculator } from "./sum";
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
@@ -12,4 +13,24 @@ test('Capitalized first letter', () => {
 
 test('Reverse a string', () => {
     expect(reverseString("hello")).toBe("olleh");
+});
+
+test('Calculator Add', () => {
+    const calc = new Calculator;
+    expect(calc.add(1, 2)).toBe(3);
+});
+
+test('Calculator Subtract', () => {
+    const calc = new Calculator;
+    expect(calc.sub(2, 1)).toBe(1);
+});
+
+test('Calculator Multiply', () => {
+    const calc = new Calculator;
+    expect(calc.mul(2, 2)).toBe(4);
+});
+
+test('Calculator Divide', () => {
+    const calc = new Calculator;
+    expect(calc.div(10, 2)).toBe(5);
 });
