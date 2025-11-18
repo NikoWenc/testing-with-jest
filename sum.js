94e3a09c3dc078eb14213b9e64a838bf98488093
NikoWenc <96979773+NikoWenc@users.noreply.github.com>
2025-11-18 20:32:09 +0800

    Create analyzeArray function and test

--- sum.js
+++ sum.js
@@ -40,3 +40,51 @@ export class Calculator {
         return num1 / num2;
     };
 };
+
+export function caesarCipher() {
+
+};
+
+export function analyzeArray(arr){
+
+    let obj = [];
+    // for avarage
+    obj.average = arr.reduce((sum, cur) => sum + cur, 0) / arr.length;
+
+    // for min
+    let min = arr[0];
+        for(i = 0; i < arr.length; i++){
+            let cur = arr[i];
+
+                if (min < cur) {
+                    min = min;
+                }else {
+                    min = cur;
+                };
+
+            obj.min = min;
+
+            console.log(obj.min);
+        };
+
+    // for max
+    let max = arr[0];
+        for(i = 0; i < arr.length; i++){
+            let cur = arr[i];
+
+                if (max > cur) {
+                    max = max;
+                }else {
+                    max = cur;
+                };
+
+            obj.max = max;
+
+            console.log(obj.max);
+        };
+
+    // for length
+    obj.length = arr.length;
+
+            return obj;
+};
--- sum.test.js
+++ sum.test.js
@@ -2,6 +2,7 @@ import { sum } from "./sum";
 import { capitalized } from "./sum";
 import { reverseString } from "./sum";
 import { Calculator } from "./sum";
+import { analyzeArray } from "./sum";
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
@@ -34,3 +35,23 @@ test('Calculator Divide', () => {
     const calc = new Calculator;
     expect(calc.div(10, 2)).toBe(5);
 });
+
+test("Array Average", () => {
+    const object = analyzeArray([1,8,3,4,2,6]);
+    expect(object.average).toBe(4);
+});
+ 
+test("Array Min", () => {
+    const object = analyzeArray([1,8,3,4,2,6]);
+    expect(object.min).toBe(1);
+});
+ 
+test("Array Max", () => {
+    const object = analyzeArray([1,8,3,4,2,6]);
+    expect(object.max).toBe(8);
+});
+ 
+test("Array Length", () => {
+    const object = analyzeArray([1,8,3,4,2,6]);
+    expect(object.length).toBe(6);
+}); 
